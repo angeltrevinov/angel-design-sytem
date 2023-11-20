a6dc5a057af5418eaa0ea5029d96574bfa92ae12
angeltrevinov <angel.trevinov@gmail.com>
2023-11-20 16:08:53 -0600

    Added library

---
+++ packages/stencil-library/stencil.config.ts
@@ -0,0 +1,33 @@
+import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
+import { sass } from '@stencil/sass';
+
+export const config: Config = {
+  namespace: 'stencil-library',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+    reactOutputTarget({
+      componentCorePackage: 'stencil-library',
+      proxiesFile: '../react-library/lib/components/stencil-generated/index.ts',
+    }),
+  ],
+  plugins: [
+    sass()
+  ],
+  testing: {
+    browserHeadless: "new",
+  },
+};
